81bace60f20bdcb147077e6833991f264e6f0089
ThiagoMoraes <thiago.pvni1@gmail.com>
2015-06-08 11:34:13 -0300

    Função Soma Binária

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,12 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textbox = new System.Windows.Forms.TextBox();
+            this.binario_button = new System.Windows.Forms.Button();
+            this.decimal_button = new System.Windows.Forms.Button();
+            this.hexa_button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button_1 = new System.Windows.Forms.Button();
+            this.button_0 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textbox
+            // 
+            this.textbox.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textbox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textbox.Location = new System.Drawing.Point(12, 12);
+            this.textbox.MaxLength = 10;
+            this.textbox.Name = "textbox";
+            this.textbox.ReadOnly = true;
+            this.textbox.Size = new System.Drawing.Size(100, 20);
+            this.textbox.TabIndex = 0;
+            this.textbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // binario_button
+            // 
+            this.binario_button.Location = new System.Drawing.Point(12, 38);
+            this.binario_button.Name = "binario_button";
+            this.binario_button.Size = new System.Drawing.Size(75, 23);
+            this.binario_button.TabIndex = 1;
+            this.binario_button.Text = "Binário";
+            this.binario_button.UseVisualStyleBackColor = true;
+            this.binario_button.Click += new System.EventHandler(this.binario_Click);
+            // 
+            // decimal_button
+            // 
+            this.decimal_button.Location = new System.Drawing.Point(12, 67);
+            this.decimal_button.Name = "decimal_button";
+            this.decimal_button.Size = new System.Drawing.Size(75, 23);
+            this.decimal_button.TabIndex = 2;
+            this.decimal_button.Text = "Decimal";
+            this.decimal_button.UseVisualStyleBackColor = true;
+            this.decimal_button.Click += new System.EventHandler(this.decimal_Click);
+            // 
+            // hexa_button
+            // 
+            this.hexa_button.Location = new System.Drawing.Point(12, 96);
+            this.hexa_button.Name = "hexa_button";
+            this.hexa_button.Size = new System.Drawing.Size(87, 23);
+            this.hexa_button.TabIndex = 3;
+            this.hexa_button.Text = "Hexadecimal";
+            this.hexa_button.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 110);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "=";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Igual_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Operacao_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(118, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Operacao_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(118, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "*";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Operacao_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(118, 99);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "/";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Operacao_Click);
+            // 
+            // button_1
+            // 
+            this.button_1.Location = new System.Drawing.Point(12, 144);
+            this.button_1.Name = "button_1";
+            this.button_1.Size = new System.Drawing.Size(28, 23);
+            this.button_1.TabIndex = 9;
+            this.button_1.Text = "1";
+            this.button_1.UseVisualStyleBackColor = true;
+            this.button_1.Click += new System.EventHandler(this.Number_Click);
+            // 
+            // button_0
+            // 
+            this.button_0.Location = new System.Drawing.Point(12, 173);
+            this.button_0.Name = "button_0";
+            this.button_0.Size = new System.Drawing.Size(28, 23);
+            this.button_0.TabIndex = 12;
+            this.button_0.Text = "0";
+            this.button_0.UseVisualStyleBackColor = true;
+            this.button_0.Click += new System.EventHandler(this.Number_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button_0);
+            this.Controls.Add(this.button_1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.hexa_button);
+            this.Controls.Add(this.decimal_button);
+            this.Controls.Add(this.binario_button);
+            this.Controls.Add(this.textbox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textbox;
+        private System.Windows.Forms.Button binario_button;
+        private System.Windows.Forms.Button decimal_button;
+        private System.Windows.Forms.Button hexa_button;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button_1;
+        private System.Windows.Forms.Button button_0;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -7,15 +7,204 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+
+  
+   
+        public bool segundo = false;
+        public string lastOperacao;
+        public bool eresultado = false;
+        public bool temvirgula = false;
+        public string operacao;
+        public string n1;
+        public string n2;
+        public string result;
+     
+
+
+
+        /*public string Converter(string a)
+        {
+            int CCount = -1;
+            R = 0;
+            
+            foreach (char b in a)
+            {
+                CCount++;
+            }
+
+            foreach (char b in a)
+            {
+                if (double.Parse(b.ToString()) < 2)
+                {
+                    R += double.Parse(b.ToString()) * Math.Pow(2, CCount);
+                }
+                }
+
+            return R.ToString();
+        }*/
+
+
         public Form1()
         {
             InitializeComponent();
+            Console.WriteLine(binSum("101001","10"));
+        }
+
+        string[] igualarNum(string n1, string n2) {
+            
+
+            if(n1.Length > n2.Length)
+            {
+                int index = n1.Length - n2.Length;
+                for(int i = 0;i < index;i++)
+                {
+                    n2 = "0" + n2;
+                }
+            }
+            else if(n2.Length > n1.Length)
+            {
+                int index = n2.Length - n1.Length;
+                for(int i = 0;i< index;i++)
+                {
+                    n1 = "0" + n1;
+                }
+            }
+            return new string[2]{n1,n2}; 
+        }
+        
+        public string binSum(string n1, string n2)
+        {
+            char[] m1 = igualarNum(n1,n2)[0].ToCharArray();
+            char[] m2 = igualarNum(n1, n2)[1].ToCharArray();
+            string overflow = "";
+            string result = "";
+            Console.WriteLine(igualarNum(n1, n2)[0] + "     " + igualarNum(n1, n2)[1]);
+            for(int i = m1.Length - 1;i >= 0;i--)
+            {
+                if (string.IsNullOrWhiteSpace(overflow))
+                {
+                    if (m1[i].Equals('0') && m2[i].Equals('0')) result += "0";
+                    if (m1[i].Equals('0') && m2[i].Equals('1')) result += "1";
+                    if (m1[i].Equals('1') && m2[i].Equals('0')) result += "1";
+                    if (m1[i].Equals('1') && m2[i].Equals('1'))
+                    {
+                        result += "0";
+                        overflow = "1";
+                    }
+                }
+                else if (m1[i].Equals('0') && m2[i].Equals('0')) 
+                { 
+                    result += "1";
+                    overflow = "";
+                }
+                else if (m1[i].Equals('0') && m2[i].Equals('1') || m1[i].Equals('1') && m2[i].Equals('0'))
+                {
+                    result = "0";
+                    overflow = "1";
+                }
+                else if(m1[i].Equals('1') && m2[i].Equals('1'))
+                {
+                    result = "1";
+                    overflow = "1";
+                }
+            }
+            result = overflow + result;
+            char[] bla = result.ToCharArray();
+            Array.Reverse(bla);
+            return new string(bla);
+        }
+
+        private void Operacao_Click(object sender, EventArgs e)
+        {
+            if (textbox.Text.Equals("NAN (NÃO É UM NÚMERO)"))
+            {
+                textbox.Text = "0";
+              //  conta.Text = "0";
+            }
+            Button xXx_operacao_xXx = sender as Button;
+            operacao = xXx_operacao_xXx.Text;
+            Debug.Print(textbox.Text + " | " + operacao);
+            n1 = textbox.Text;
+            Debug.Print(n1 + " | " + operacao);
+            temvirgula = false;
+            segundo = true;
+            eresultado = false;
+            lastOperacao = "";
+        
+        
+        
+        
+        }
+
+
+        private void binario_Click(object sender, EventArgs e)
+        {
 
         }
-    }
-}
+
+        private void decimal_Click(object sender, EventArgs e)
+        {
+           // textbox.Text = Converter(textbox.Text);
+            
+        }
+
+       
+        private void Number_Click(object sender, EventArgs e)
+        {
+            Button xXx_digito_xXx = sender as Button;
+            lastOperacao = "";
+            if (textbox.Text.Length <= 15)
+            {
+                if (segundo == true) { textbox.Text = ""; }
+                segundo = false;
+                if (textbox.Text != "0")
+                   {
+                        textbox.Text = textbox.Text + xXx_digito_xXx.Text;                   
+                   }
+                    else
+                    {
+                        if (eresultado == false)
+                        {
+                            textbox.Text = textbox.Text + xXx_digito_xXx.Text;
+                        }
+                        else
+                        {
+                            textbox.Text = xXx_digito_xXx.Text;
+                            eresultado = false;
+                        }
+                    }
+                }
+                else
+                {
+                    textbox.Text = xXx_digito_xXx.Text;
+                }
+            }
+
+        private void Igual_Click(object sender, EventArgs e)
+        {
+            switch (operacao) { 
+                case "+":
+                    eresultado = true;
+                   /* if (lastOperacao == "") { n2 = Convert.ToDouble(textbox.Text); }
+                    else { n1 = Convert.ToDouble(textbox.Text); }*/
+                    //lastOperacao = operacao;
+                    igualarNum(n1,n2);
+                    textbox.Text = binSum(n1, n2);
+                    Debug.Print(n1 + " | " + operacao + " | " + n2 + " | " + result);
+                    //conta.Text = primeironm + " " + operacao + " " + segundonm + " = " + result;
+                    break;
+            }
+        }
+
+
+            } 
+        }
+    
+
