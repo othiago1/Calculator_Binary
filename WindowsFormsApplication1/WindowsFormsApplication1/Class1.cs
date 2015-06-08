fbc2d93d47ec54be548b3d789db45d7b1eec0e1b
ThiagoMoraes <thiago.pvni1@gmail.com>
2015-06-08 11:33:39 -0300

    Inicio do Conversor

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Class1
+    {
+        public string Converter(string a){
+            int CCount = -1;
+            double R = 0;
+            foreach (char potato in a){
+                CCount++;
+            }
+
+            foreach (char potato in a)
+            {
+                R += double.Parse(potato.ToString()) * Math.Pow(2,CCount);
+            }
+
+            return R.ToString();
+        }
+    }
+}
